97660362417a472faa8ee8b1a9fb921e02bcb45a
sahawaenya <saha.waenya@gmail.com>
2023-03-30 01:29:36 +0700

    check point

--- routes/category.js
+++ routes/category.js
@@ -2,8 +2,7 @@ const Controller = require("../controllers/category");
 
 const routes = require("express").Router();
 
-if (process.env.NODE_ENV !== "production")
-  routes.post("/", Controller.createCategory);
+routes.post("/", Controller.createCategory);
 routes.get("/:CategoryId", Controller.getCategoryItems);
 
 module.exports = routes;
--- routes/image.js
+++ routes/image.js
@@ -2,7 +2,6 @@ const Controller = require("../controllers/image");
 
 const routes = require("express").Router();
 
-if (process.env.NODE_ENV !== "production")
-  routes.post("/", Controller.insertImage);
+routes.post("/", Controller.insertImage);
 
 module.exports = routes;
