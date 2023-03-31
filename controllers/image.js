97a823ca503e44fa3328780752f3d886b93fc583
sahawaenya <saha.waenya@gmail.com>
2023-03-31 17:06:44 +0700

    add logger

--- controllers/image.js
+++ controllers/image.js
@@ -1,23 +1,28 @@
 const sharp = require("sharp");
-const { loggerError } = require("../helpers/loggerDebug");
+const { loggerError, loggerInfo } = require("../helpers/loggerDebug");
 const { Image } = require("../models");
 
 module.exports = class Controller {
   static async insertImage(req, res, next) {
     try {
+      loggerInfo("Inserting image");
       const imagesData = req.files.images;
-      const { CategoryId } = req.body;
 
-      console.log(Array.isArray(imagesData));
+      loggerInfo(Object.keys(imagesData));
+      const { CategoryId } = req.body;
+      loggerInfo(CategoryId);
+      loggerInfo(Array.isArray(imagesData));
       if (!Array.isArray(imagesData)) {
         const convertedImage = await sharp(imagesData.data)
           .toFormat("webp")
           .toBuffer();
+        loggerInfo("image converted");
         const result = await Image.create({
           name: imagesData.name,
           file: convertedImage,
           CategoryId,
         });
+        loggerInfo("image inserted to database!");
         res.status(200).json(result.name + " has been added");
       } else {
         const data = await Promise.all(
